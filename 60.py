3e9179c3816bbce076f54f284ecd479bf238f7f4
Saba <46112197+Elmo33@users.noreply.github.com>
2021-05-14 10:33:39 +0400

    Create 60.py

---
+++ 60.py
@@ -0,0 +1,14 @@
+vertices = int(input())
+total = 0
+
+x, y = map(int, input().split())
+x1 = x
+y1 = y
+
+for i in range(vertices - 1):
+    x2, y2 = map(int, input().split())
+    total = total + (x1 + x2) * (y2 - y1)
+    x1 = x2
+    y1 = y2
+total = total + (x + x2) * (y - y2)
+print('{:.3f}'.format(abs(total)/2))
