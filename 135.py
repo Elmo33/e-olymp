c8fd100e90ed72569dcd3283194c6f5d988648c1
Saba <46112197+Elmo33@users.noreply.github.com>
2021-05-14 10:32:51 +0400

    Create 135.py

---
+++ 135.py
@@ -0,0 +1,14 @@
+def gcd(n, m):
+    if m == 0:
+        return n
+    return gcd(m, n % m)
+
+
+test = input()
+
+A = list(map(int, input().split()))
+
+lcm = 1
+for i in A:
+    lcm = lcm*i//gcd(lcm,i)
+print(lcm)
